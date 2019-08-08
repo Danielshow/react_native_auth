b7326367172609ceb277f06e3c269bc8eb99b085
danielshow <danielshotonwa53@gmail.com>
2019-08-08 22:37:47 +0100

    initialize application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>OK</Text>
+      <Text>OK</Text>
+      <Text>OK</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/components/common/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Header = ({ headerText }) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}> {headerText} </Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 25,
+  },
+
+  viewStyle: {
+    backgroundColor: "pink",
+    height: 80,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+};
+
+export default Header;
