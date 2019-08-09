f7e349d138dd5110e5fed6e699e3dd17ef6671ab
danielshow <danielshotonwa53@gmail.com>
2019-08-09 01:36:19 +0100

    render logout button based on condtion

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { View } from 'react-native';
 import firebase from "firebase";
 import { Header } from "./components/common";
 import LoginForm from './components/LoginForm';
-
+import { Button, Spinner } from "./components/common";
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  };
+
   componentDidMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCeR5o-QBuYH224xi2rKzJG7_OYrtti6y0',
@@ -16,15 +20,59 @@ class App extends Component {
       messagingSenderId: '137785591211',
       appId: '1:137785591211:web:32f103fdbde0ec26',
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          loggedIn: true
+        });
+      } else {
+        this.setState({
+          loggedIn: false
+        });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={styles.buttonStyle}>
+            <Button>Logout</Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.loadingStyle} >
+            <Spinner />
+          </View>
+        );
+    }
   }
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
 
+const styles = {
+  buttonStyle: {
+    marginTop: 30,
+    height: 40,
+  },
+
+  loadingStyle: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 export default App;
